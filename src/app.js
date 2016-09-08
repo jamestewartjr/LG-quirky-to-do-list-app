cbfd85b9546ba0e627044de3414b7ff1b3dcb37f
nicosesma <nicosm310@gmail.com>
2016-09-07 18:24:39 -0700

    WIP Delete, broken, not working

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const loadListingPage = () => {
     listingTemplate({ todos })
 
   bindAddButton()
+  bindDeleteButton()
   bindTodoEdit( loadListingPage )
 }
 
@@ -22,6 +23,19 @@ const bindAddButton = () => {
     const { todos } = database.createTodo( input.value )
 
     input.value = ''
+    loadListingPage()
+  })
+}
+
+const bindDeleteButton = () => {
+  const deleteButton = document.querySelector( '.task button' )
+  console.log('1', deleteButton)
+  deleteButton.addEventListener( 'click', event => {
+    const input = document.getElementById( 'delete-action')
+    console.log('dataid', )
+    console.log('input',input)
+
+    const { todos } = database.deleteTodo( input )
     loadListingPage()
   })
 }
--- src/database/delete.js
+++ src/database/delete.js
@@ -4,7 +4,8 @@ import allTodos from './list'
 const deleteTodo = id => {
   const data = getData()
   const index = data.todos.findIndex( todo => todo.id === id )
-
+  // removeItem(index)
+  console.log('dataDel', data)
   saveData( Object.assign( {}, data, 
     { todos: 
       [
@@ -13,6 +14,7 @@ const deleteTodo = id => {
       ]
     })
   )
+
 }
 
 export default deleteTodo
